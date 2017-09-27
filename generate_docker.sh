ec4f72771edd791390293bfdd4f0c07e6cef86d6
tjiagoM <tiago.manuel@gmail.com>
2017-09-27 16:45:43 +0100

    dockerfile generation code, basic jupyter possibility

--- generate_docker.sh
+++ generate_docker.sh
@@ -1,21 +1,18 @@
 # Generate Dockerfile.
 docker run --rm kaczmarj/neurodocker:master generate \
---base debian:stretch --pkg-manager apt \
---install git vim \
---fsl version=5.0.9 \
+--base debian:stretch-slim --pkg-manager apt \
 --user=neuro \
 --miniconda env_name=neuro \
-            conda_opts="--channel vida-nyu" \
             conda_install="python=3.6.1 numpy pandas reprozip traits nipype networkx jupyter" \
             add_to_path=true \
 --miniconda env_name=py27 \
             conda_install="python=2.7" \
 --expose 4000 80 \
---label FOO=BAR BAZ=CAT \
 --workdir /home/neuro \
---instruction='ADD helloword.py /home/neuro' \
---cmd python helloword.py \
+--instruction='ADD helloworld.py /home/neuro' \
+--instruction='RUN python helloworld.py' \
 --no-check-urls > generated.Dockerfile
 
+docker build -t neuronewbies -f generated.Dockerfile .
 
-docker build -t initgraphmri -f generated.Dockerfile .
+docker run -it -p 8888:8888 neuronewbies
